debf63f26d30e9688c06d9477135de91878542ac
DevAkshat21 <akshat.2108.07@gmail.com>
2026-07-18 01:25:49 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -26,6 +26,21 @@ int main(int argc, char *argv[]) {
     printf("%s\n", command+5);
   }
 
+  else if(strncmp(command, "type ",5)==0){
+    if(strncmp(command+5, "echo" ,4)==0){
+      printf("%s is a shell builtin \n" ,command+5);
+    }
+    else if(strncmp(command+5, "type " ,4)==0){
+      printf("%s is a shell builtin \n" ,command+5);
+    }
+    else if(strncmp(command+5, "exit " ,4)==0){
+      printf("%s is a shell builtin \n" ,command+5);
+    }
+    else{
+      printf("%s: not found\n",command+5);
+    }
+  }
+
   // Prints the "<command>: command not found" message
   else{
   printf("%s: command not found\r\n", command);
