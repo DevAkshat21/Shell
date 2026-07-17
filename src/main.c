84f83d22573868f6e21458e401c3217c79200aae
DevAkshat21 <akshat.2108.07@gmail.com>
2026-07-18 02:55:42 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -6,9 +6,13 @@ int main(int argc, char *argv[]) {
 
   // Flush after every printf
   setbuf(stdout, NULL);
+
   int i;
   i = 1;
+
+
   while (i == 1)
+
   {
   printf("$ ");
 
@@ -18,28 +22,44 @@ int main(int argc, char *argv[]) {
 
   // Remove the trailing newline
   command[strcspn(command, "\r\n")] = '\0';
-  if (strcmp(command,"exit") == 0){
+
+  char *builtin = strtok(command," ");
+  char *arg = strtok(command,"");
+
+  if (strcmp(builtin,"exit") == 0){
     i = 0;
   }
 
-  else if (strncmp(command, "echo ", 5) == 0) {
+  else if (strcmp(builtin,"echo ") == 0) {
     printf("%s\n", command+5);
   }
 
-  else if(strncmp(command, "type ",5)==0){
-    if(strncmp(command+5, "echo" ,4)==0){
-      printf("%s is a shell builtin \n" ,command+5);
-    }
-    else if(strncmp(command+5, "type " ,4)==0){
-      printf("%s is a shell builtin \n" ,command+5);
-    }
-    else if(strncmp(command+5, "exit " ,4)==0){
-      printf("%s is a shell builtin \n" ,command+5);
-    }
+  else if(strcmp(builtin,"type")==0){
+    
+    if (!strcmp(arg, "type") ||
+    !strcmp(arg, "echo") ||
+    !strcmp(arg, "exit")) {
+    printf("%s is a shell builtin\n", arg);}
+
     else{
-      printf("%s: not found\n",command+5);
+      printf("%s: not found\n",arg);
     }
   }
+
+  // else if(strncmp(command, "type ",5)==0){
+  //   if(strncmp(command+5, "echo" ,4)==0){
+  //     printf("%s is a shell builtin \n" ,command+5);
+  //   }
+  //   else if(strncmp(command+5, "type " ,4)==0){
+  //     printf("%s is a shell builtin \n" ,command+5);
+  //   }
+  //   else if(strncmp(command+5, "exit " ,4)==0){
+  //     printf("%s is a shell builtin \n" ,command+5);
+  //   }
+  //   else{
+  //     printf("%s: not found\n",command+5);
+  //   }
+  // }
 
   // Prints the "<command>: command not found" message
   else{
