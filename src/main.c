810d0ccd167c9dee0d9b8acd12813aa036e631cd
DevAkshat21 <akshat.2108.07@gmail.com>
2026-07-19 13:59:20 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,76 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <io.h>
+char *find_path(char *arg) {
+    static char full_path[1024];
+
+    char path_copy[4096];
+    strcpy(path_copy, getenv("PATH"));
+
+    // Windows PATH is separated by ';'
+    char *dir = strtok(path_copy, ";");
+
+    while (dir != NULL) {
+
+        strcpy(full_path, dir);
+        strcat(full_path, "\\");
+        strcat(full_path, arg);
+
+        // Does this file exist?
+        if (_access(full_path, 0) == 0) {
+            return full_path;
+        }
+
+        dir = strtok(NULL, ";");
+    }
+
+    return NULL;
+}
 
 int main(int argc, char *argv[]) {
 
-  // Flush after every printf
-  setbuf(stdout, NULL);
+    setbuf(stdout, NULL);
 
-  int i;
-  i = 1;
+    int i = 1;
 
+    while (i == 1) {
 
-  while (i == 1)
+        printf("$ ");
 
-  {
-  printf("$ ");
+        char command[1024];
+        fgets(command, sizeof(command), stdin);
 
-  // Captures the user's command in the "command" variable
-  char command[1024];
-  fgets(command, sizeof(command), stdin);
+        command[strcspn(command, "\r\n")] = '\0';
 
-  // Remove the trailing newline
-  command[strcspn(command, "\r\n")] = '\0';
+        char *builtin = strtok(command, " ");
+        char *arg = strtok(NULL, "");
 
-  char *builtin = strtok(command," ");
-  char *arg = strtok(NULL,"");
+        if (builtin == NULL)
+            continue;
 
-  if (strcmp(builtin,"exit") == 0){
-    i = 0;
-  }
+        // Exit
+        if (strcmp(builtin, "exit") == 0) {
+            i = 0;
+        }
 
-  else if (strcmp(builtin,"echo") == 0) {
-    printf("%s\n", command+5);
-  }
+        // Echo
+        else if (strcmp(builtin, "echo") == 0) {
+            printf("%s\n", arg ? arg : "");
+        }
 
-  else if(strcmp(builtin,"type")==0){
-    
-    if (!strcmp(arg, "type") ||
-    !strcmp(arg, "echo") ||
-    !strcmp(arg, "exit")) {
-    printf("%s is a shell builtin\n", arg);}
+        // Type
+        else if (strcmp(builtin, "type") == 0) {
 
-    else{
-      printf("%s: not found\n",arg);
+            if (!strcmp(arg, "type") ||
+                !strcmp(arg, "echo") ||
+                !strcmp(arg, "exit")) {
+
+                printf("%s is a shell builtin\n", arg);
+            }
+            else {
+            char *path = find_path(arg);
+
+            if (path != NULL) {
+                printf("%s is %s\n", arg, path);
+            } else {
+                printf("%s: not found\n", arg);
+                }
+            }
+        }
+
+        // Unknown command
+        else {
+            printf("%s: command not found\n", builtin);
+        }
     }
-  }
 
-  // else if(strncmp(command, "type ",5)==0){
-  //   if(strncmp(command+5, "echo" ,4)==0){
-  //     printf("%s is a shell builtin \n" ,command+5);
-  //   }
-  //   else if(strncmp(command+5, "type " ,4)==0){
-  //     printf("%s is a shell builtin \n" ,command+5);
-  //   }
-  //   else if(strncmp(command+5, "exit " ,4)==0){
-  //     printf("%s is a shell builtin \n" ,command+5);
-  //   }
-  //   else{
-  //     printf("%s: not found\n",command+5);
-  //   }
-  // }
-
-  // Prints the "<command>: command not found" message
-
-  else{
-  printf("%s: command not found\r\n", command);
-  }
-
-  }
-  
-
-
-
-  return 0;
+    return 0;
 }
